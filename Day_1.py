474be810cafa6ffe412ba8acd325194b04954280
“Vishalappu21” <vishalanbu582000@gmail.com>
2025-09-05 22:54:21 +0530

    Input Project

---
+++ Day_1.py
@@ -0,0 +1,32 @@
+# print("vishal prasnna\nson of anbalagan")
+# print("Vishal" + " " +"Prasanna")
+#
+# # Fix the code below 👇
+#
+# print("Notes from Day 1")
+# print("The print statement is used to output strings")
+# print("Strings are strings of characters")
+# print("String Concatenation is done with the + sign")
+# print(("New lines can be created with a \ and the letter n"))
+
+#
+# Name = input("What is Your Name?")
+# answer = "Hello" + Name +"!"
+# print(answer)
+# print(len(answer))
+#
+# glass1 = "milk"
+# glass2 = "water"
+# temp = glass1
+# glass1 = glass2
+# glass2 = temp
+# print(glass1)
+# print(glass2)
+
+#DAY_1_PROJECT
+print("Welcome to my 100 days project Program")
+city = input("what is the name of the city you were grew up?")
+print(city)
+pet_name = input("What is your name of the Pet?")
+print(pet_name)
+print(city+pet_name)
