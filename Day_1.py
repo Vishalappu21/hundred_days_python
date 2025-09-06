3789d164b66d9c6c03979bfce040cca36d439776
“Vishalappu21” <vishalanbu582000@gmail.com>
2025-09-06 19:54:46 +0530

    TIP Calculator project Day_2

--- Day_1.py
+++ Day_1.py
@@ -1,27 +1,27 @@
-# print("vishal prasnna\nson of anbalagan")
-# print("Vishal" + " " +"Prasanna")
-#
-# # Fix the code below 👇
-#
-# print("Notes from Day 1")
-# print("The print statement is used to output strings")
-# print("Strings are strings of characters")
-# print("String Concatenation is done with the + sign")
-# print(("New lines can be created with a \ and the letter n"))
+print("vishal prasnna\nson of anbalagan")
+print("Vishal" + " " +"Prasanna")
 
-#
-# Name = input("What is Your Name?")
-# answer = "Hello" + Name +"!"
-# print(answer)
-# print(len(answer))
-#
-# glass1 = "milk"
-# glass2 = "water"
-# temp = glass1
-# glass1 = glass2
-# glass2 = temp
-# print(glass1)
-# print(glass2)
+
+
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print(("New lines can be created with a \ and the letter n"))
+
+
+Name = input("What is Your Name?")
+answer = "Hello" + Name +"!"
+print(answer)
+print(len(answer))
+
+glass1 = "milk"
+glass2 = "water"
+temp = glass1
+glass1 = glass2
+glass2 = temp
+print(glass1)
+print(glass2)
 
 #DAY_1_PROJECT
 print("Welcome to my 100 days project Program")
